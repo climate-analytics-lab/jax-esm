31168621dbf1a2eb8a690dc94f1898fd09325849
Tien-Yiao Hsu <meteorologytoday@gmail.com>
2025-10-27 11:40:00 +0100

    Output predictions into netcdf files

--- examples/jesm_JCM_SlabOceanModel.py
+++ examples/jesm_JCM_SlabOceanModel.py
@@ -1,12 +1,18 @@
 """Example of coupling jax-gcm with a simple slab ocean model."""
 
 if __name__ == "__main__":
-       
+
+    from pathlib import Path       
     import jcm
     topo_file = (Path(jcm.__file__).parent / "data/bc/t30/clim/boundaries_daily.nc").resolve()
     
     from jax_esm.coupling.couplers.CoupledJCMSlabOceanModel import CoupledJCMSlabOceanModel
     from datetime import datetime
+    
+    output_dir = Path("output").resolve()
+
+    print("Output dir: ", str(output_dir))
+    output_dir.mkdir(exist_ok=True, parents=True) 
 
     # Creating model
     model = CoupledJCMSlabOceanModel(
@@ -14,7 +20,7 @@ if __name__ == "__main__":
         horizontal_resolution = 31,
         JCM_layers = 8,
         coupling_timestep = 86400.0,
-        JCM_substeps = 24,
+        JCM_substeps = 48,
         SlabOceanModel_substeps = 1,
         start_datetime = datetime(year=2001, month=1, day=1),
     )
@@ -26,11 +32,16 @@ if __name__ == "__main__":
     final_state, predictions = model.run(
         init_cplstate = initial_state,
         start_time = 0.0,
-        end_time = 86400.0 * 5,
+        end_time = 86400.0 * 20,
         jax_scan = False,
     )
 
     # Convert output into xarray
-    ds_output = model.predictions_to_xarray(predictions)
+    output_dict = model.predictions_to_xarray(predictions)
+   
     
-     
+
+    for component_name, ds in output_dict.items():
+        output_file = output_dir / f"JESM-JCM_SOM-{component_name:s}.nc"
+        print("Output file: ", str(output_file))
+        ds.to_netcdf(output_file, engine="netcdf4")
