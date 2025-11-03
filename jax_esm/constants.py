36a97ed6d3a589bd9c04d19d914273148d277136
Tien-Yiao Hsu <meteorologytoday@gmail.com>
2025-11-03 12:53:53 +0100

    Rename constants to natural naming

--- jax_esm/constants.py
+++ jax_esm/constants.py
@@ -5,8 +5,8 @@ g0 = 9.81  # Gravitational acceleration on Earth (m/s/s)
 stephan_boltzmann_const = 5.67e-8
 solar_const = 1367.0
 
-atm_column_mass = 1e4   # Mass of entire atmosphere column per unit area (kg / m^2)
-atm_cp = 1004.0 # Specific heat at constant pressure (J/K/kg)
+atmosphere_column_mass = 1e4   # Mass of entire atmosphere column per unit area (kg / m^2)
+atmosphere_specific_heat_capacity_under_constant_pressure = 1004.0 # Specific heat at constant pressure (J/K/kg)
 
-ocn_rho = 1025.0  # Seawater density (kg/m^3)
-ocn_cp = 3985.0   # Seawater specific heat capacity (J/kg/K)
+ocean_density = 1025.0  # Seawater density (kg/m^3)
+ocean_specific_heat_capacity = 3985.0   # Seawater specific heat capacity (J/kg/K)
