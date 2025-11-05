c26f1f2af11fc6b43a3e703197fe039a5d38e920
Tien-Yiao Hsu <tienyiao@pik-potsdam.de>
2025-11-05 13:50:53 +0100

    Add in interpolability of JCM+SlabOceanModel

--- jax_esm/coupling/couplers/CoupledJCMSlabOceanModel.py
+++ jax_esm/coupling/couplers/CoupledJCMSlabOceanModel.py
@@ -14,6 +14,7 @@ from jax_esm.coupling.coupler import (
 import jcm
 from pathlib import Path
 
+from jax_esm.utils.bilinear_interp import BilinearInterpolator
 import jax.numpy as jnp
 
 
@@ -99,13 +100,72 @@ class CoupledJCMSlabOceanModel(Coupler):
     ):
 
 
+        atm_model = components["atm"]
+        ocn_model = components["ocn"]
+        
+        atm_domain = atm_model.config.domain
+        ocn_domain = ocn_model.config.domain
+
+        if atm_domain.grid_specification.grid_type in ["JCM", "Veros"] and ocn_domain.grid_specification.grid_type in ["JCM", "Veros"]:
+ 
+            def find_latitude_longitude(domain: Domain):
+                
+                T_grid = domain.grids["T"] 
+
+                lat_dim_idx = next( i for i, axis_name in enumerate(T_grid.axis_names) if axis_name == "latitude")
+                lon_dim_idx = next( i for i, axis_name in enumerate(T_grid.axis_names) if axis_name == "longitude")
+
+                return T_grid.axis_values[lat_dim_idx], T_grid.axis_values[lon_dim_idx] 
+            
+            atm_latitude, atm_longitude = find_latitude_longitude(atm_domain)
+            ocn_latitude, ocn_longitude = find_latitude_longitude(ocn_domain)
+  
+            interpolator_atm_to_ocn = BilinearInterpolator(
+                longitude_source_deg = atm_longitude,
+                latitude_source_deg = atm_latitude,
+                longitude_target_deg = ocn_longitude,
+                latitude_target_deg = ocn_latitude,
+                periodic_longitude = True,
+                #target_mask: Optional[Array] = None,
+                #source_mask: Optional[Array] = None,
+                fill_value = 0.0,
+            )
+ 
+            interpolator_ocn_to_atm = BilinearInterpolator(
+                longitude_target_deg = atm_longitude,
+                latitude_target_deg = atm_latitude,
+                longitude_source_deg = ocn_longitude,
+                latitude_source_deg = ocn_latitude,
+                periodic_longitude = True,
+                #target_mask: Optional[Array] = None,
+                #source_mask: Optional[Array] = None,
+                fill_value = 0.0,
+            )
+
+            atm_latlon = atm_domain.grid_specification.grid_type == "Veros"
+            ocn_latlon = ocn_domain.grid_specification.grid_type == "Veros"
+           
+        else:
+            raise Exception("Currently only support grids in JCM and Veros.")
+
+
+        def conditional_transpose(a, flag):
+            return jnp.transpose(a) if flag else a
+ 
         def transformation(state_group, forcing_group):
 
             atm = state_group["atm"]
             ocn = state_group["ocn"]
            
-            forcing_group["atm"].scalar.sea_surface_skin_temperature = ocn.prog.T
-            forcing_group["ocn"].flux.total_heat_flux = atm.phydata.surface_flux.hfluxn.sum(axis=-1)
+            forcing_group["atm"].scalar.sea_surface_skin_temperature = conditional_transpose(
+                interpolator_ocn_to_atm.apply_scalar( conditional_transpose( ocn.prog.T, not ocn_latlon ) ),
+                not atm_latlon
+            )
+            
+            forcing_group["ocn"].flux.total_heat_flux = conditional_transpose(
+                interpolator_atm_to_ocn.apply_scalar( conditional_transpose( atm.phydata.surface_flux.hfluxn.sum(axis=-1), not atm_latlon) ),
+                not ocn_latlon
+            )
 
             return forcing_group
 
