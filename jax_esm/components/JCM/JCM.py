579d55919d7e6b28055d7df71da7759c0c9892f7
Tien-Yiao Hsu <tienyiao@pik-potsdam.de>
2025-11-05 11:42:54 +0100

    1. Incorporate Domain into JCM 2. Incorporate forcing into JCM step_function

--- jax_esm/components/JCM/JCM.py
+++ jax_esm/components/JCM/JCM.py
@@ -18,18 +18,31 @@ from jax_esm.components.base import (
     Component,
     ComponentConfig,
     AbstractComponentState,
+    create_component_forcing_class,
+    create_field_group_class,
 )
 
 
 from jcm.physics_interface import dynamics_state_to_physics_state, physics_state_to_dynamics_state
-from dinosaur import primitive_equations, primitive_equations_states
 from jcm.physics_interface import PhysicsState
 from jcm.model import Predictions
 
+import dinosaur
+from dinosaur import primitive_equations, primitive_equations_states
+
 from jax_esm.utils.bulk_op import stack_objects, mean_leaf
+from jax_esm.components.domain import Domain
 
 import tree_math
 from typing import Any
+
+from jcm.geometry import get_coords
+
+# This is a temporary solution to jcm's problem: some of the array's initiated
+# by jcm is int32, but it will change to float32 after step_function. This causes
+# jax.lax.scan to fail due to data type inconsistency.
+def asfloat64(tree):
+    return jax.tree_util.tree_map(lambda arr: arr.astype(jnp.float64), tree)
 
 @tree_math.struct
 @dataclass
@@ -43,6 +56,54 @@ class JCM(Component):
     """
     This is a class wrapping JCM.
     """
+
+    @classmethod
+    def generate_default_configuration(
+        cls,
+        grid_specification:str="JCM::T31",
+        layers:int = 8,
+        dict_form = False,
+        topography_file=None,
+        mask_file=None,
+    ):
+
+        domain = Domain.from_grid_specification(
+            grid_specification,
+            topography_file = topography_file,
+            mask_file = mask_file,
+        )
+
+        domain.meta["layers"] = layers
+
+        config_dict = dict(
+            name="default_config",
+            timestep = (timestep := 86400.0),
+            start_dt = datetime(year=2025, month=1, day=1),
+            substeps = (substeps := 2),
+            save_interval = timestep / substeps,
+            domain = domain,
+            params=dict(
+                
+            ),
+        )
+
+        if dict_form:
+            return config_dict
+        else:
+            return ComponentConfig(**config_dict)
+        
+        
+    @classmethod
+    def generate_default_model(cls, grid_specification:str="JCM::T31", layers:int=8, topography_file=None, mask_file=None):
+
+        return cls(
+            cls.generate_default_configuration(
+                grid_specification=grid_specification,
+                layers = layers,
+                topography_file=topography_file,
+            )
+        )
+
 
     
     def __init__(
@@ -61,18 +122,36 @@ class JCM(Component):
         self.coupling_timestep = config.timestep  # in secs
         self.save_interval = config.save_interval # in secs
         self.substeps = config.substeps
-        config_speedy = dict(
+        config_jcm = dict(
             time_step = self.coupling_timestep / self.substeps / 60.0, # in minutes
+            orography = self.config.domain.topography,
+            spectral_truncation = self.config.domain.meta["horizontal_resolution"],
+            layers = self.config.domain.meta["layers"],
         )
 
-        if "boundaries" in config.params and config.params["boundaries"] is not None:
-            boundaries = config.params["boundaries"]
-            config_speedy["orography"] = boundaries.orog
-
-        self.orog = boundaries.orog
-        self.model = Model(**config_speedy) 
-
         
+        self.model = Model(**config_jcm) 
+
+        D3_nodal_shape = self.model.coords.nodal_shape
+        D2_nodal_shape = D3_nodal_shape[1:]
+ 
+        self.component_forcing_class = create_component_forcing_class(
+            cls_name = "forcing",
+            flux_cls = create_field_group_class(
+                cls_name = "flux",
+                fields = [
+                    ("sensible_heat_flux", float, D2_nodal_shape),
+                    ("latent_heat_flux", float, D2_nodal_shape),
+                ],
+            ),
+            scalar_cls = create_field_group_class(
+                cls_name = "scalar",
+                fields = [
+                    ("sea_surface_skin_temperature", float, D2_nodal_shape),
+                ],
+            ),
+        )
+       
     
     def initialize(
         self,
@@ -96,7 +175,7 @@ class JCM(Component):
                 model.initial_state = dynamics_state_to_physics_state(model._final_modal_state, model.primitive)
             
         model.start_date = start_date
-        model.boundaries = default_boundaries(self.model.coords.horizontal, self.model.orography)
+        model.boundaries = boundaries or default_boundaries(self.model.coords.horizontal)
 
         # The following code is a solution to have an initial value for phydata by stepping the model one time.
         # The returned phydata is then used for the initial value.
@@ -107,29 +186,23 @@ class JCM(Component):
             date       = model._date_from_sim_time(jnp.array(model._final_modal_state.sim_time)),
         )
 
-        # This is a temporary solution to jcm's problem: some of the array's initiated
-        # by jcm is int32, but it will change to float32 after step_fn. This causes
-        # jax.lax.scan to fail due to data type inconsistency.
-        def asfloat32(tree):
-            return jax.tree_util.tree_map(lambda arr: arr.astype(jnp.float32), tree)
         
         return JCMState(
-            prog     = asfloat32(model.initial_state),
-            phydata  = asfloat32(init_phydata),
+            prog     = asfloat64(model.initial_state),
+            phydata  = asfloat64(init_phydata),
             metadata = model._final_modal_state,
         )
 
-    def gen_step_fn(self):
+    def generate_step_function(self, jitted: bool = True):
        
-        @jax.jit
-        def step_fn(cpl, t):
-           
+        def step_function(state, forcing, t):
+          
             atm_boundary = self.model.boundaries.copy(
-                tsea = cpl.ocn.prog.T
+                tsea = jnp.repeat(jnp.expand_dims(forcing.scalar.sea_surface_skin_temperature, axis=2), axis=2, repeats=365),
             )
-
+                
             new_atm_modal_state, predictions = self.model.run_from_state(
-                initial_state = cpl.atm.metadata,
+                initial_state = state.metadata,
                 save_interval = self.save_interval / 86400.0, # in days
                 total_time = self.coupling_timestep / 86400.0, # in days
                 boundaries = atm_boundary,
@@ -138,12 +211,12 @@ class JCM(Component):
             # phydata is a stacked object, so I take the mean here.
             # Howwever, this action will be done by jcm in the new jcm PR.
             return JCMState(
-                prog    = mean_leaf(predictions.dynamics, axis=0),
-                phydata = mean_leaf(predictions.physics, axis=0),
+                prog    = asfloat64(mean_leaf(predictions.dynamics, axis=0)),
+                phydata = asfloat64(mean_leaf(predictions.physics, axis=0)),
                 metadata = new_atm_modal_state,
             ), predictions
             
-        return step_fn
+        return jax.jit(step_function) if jitted else step_function
 
     def predictions_to_xarray(
         self,
