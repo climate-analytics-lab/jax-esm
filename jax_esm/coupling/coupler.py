0d437a0ab1e59056a84c3b11605ddbb6353d986f
Tien-Yiao Hsu <tienyiao@pik-potsdam.de>
2025-11-10 22:45:31 +0100

    Adjust coupler to fit in new pattern. Also, add in code to loop step functions within a coupling_timestep

--- jax_esm/coupling/coupler.py
+++ jax_esm/coupling/coupler.py
@@ -1,5 +1,6 @@
 """Main coupler class for Earth system model coupling."""
 
+from functools import partial
 import time
 from typing import Callable, Dict, List, Optional, Tuple
 
@@ -16,7 +17,6 @@ from jax_esm.utils.bulk_op import unwrap_leading_dims, stack_objects
 
 # Python Equivalent. See https://docs.jax.dev/en/latest/_autosummary/jax.lax.scan.html
 def adhoc_scan(f, init, xs=None, length=None):
-    
     if xs is None:
         xs = [1] * length
     carry = init
@@ -59,13 +59,13 @@ class Coupler:
         
         # Extract component timesteps
         component_timesteps = {
-            name: comp.timestep for name, comp in components.items()
+            name: comp.config.timestep for name, comp in components.items()
         }
 
         # Check the compatibility of timestep
-        for component_name, component in self.components.items():
-            if self.coupling_timestep % component.timestep != 0.0:
-                raise ValueError(f"Timestep of {component_name:s} ({component.timestep:f}) is not compatible with coupling timestep {self.coupling_timestep:f}.")
+        for component_name, component_timestep in component_timesteps.items():
+            if self.coupling_timestep % component_timestep != 0.0:
+                raise ValueError(f"Timestep of {component_name:s} ({component_timestep:f}) is not compatible with coupling timestep {self.coupling_timestep:f}.")
  
         self.forcing_mapper = forcing_mapper
 
@@ -100,10 +100,31 @@ class Coupler:
         Returns:
             New states after one coupling timestep
         """
+        
+        scan_func = jax.lax.scan if jitted else adhoc_scan
 
         # Get step functions for the three components
         step_functions = { component_name : component.generate_step_function(jitted = jitted) for component_name, component in self.components.items() }
 
+
+        step_functions = {}
+        for component_name, component in self.components.items():
+            step_function = component.generate_step_function(jitted=jitted)
+            # Closure in a loop is used. Using partial together.
+            def looped_step_function(state, forcing, t, step_function, component):
+                ts = t + component.config.timestep * jnp.arange(int(self.coupling_timestep / component.config.timestep))
+                def wrapped_step_function(bundle, t):
+                    new_state, history = step_function(bundle["state"], bundle["forcing"], t)
+                    return dict(state=new_state, forcing=bundle["forcing"]), history
+                _carry, _predictions = scan_func(
+                    wrapped_step_function,
+                    dict(state=state, forcing=forcing),
+                    xs = ts,
+                )
+                _predictions = unwrap_leading_dims(_predictions, first_n_dim=2)
+                return _carry["state"], _predictions
+            step_functions[component_name] = partial(looped_step_function, step_function=step_function, component=component)
+        
         def step_function(coupled_state, t):
             
             # Compute forcing
@@ -124,47 +145,35 @@ class Coupler:
 
     def run(
         self,
-        init_cplstate : Dict[str, ComponentState],
+        init_coupled_state : Dict[str, ComponentState],
         start_time    : float,
         end_time      : float,
         jax_scan: bool = True,
         save_interval_steps = 1,
     ):
 
-        coupler = self
-        
         _start_time = time.time()
         scan_func = jax.lax.scan if jax_scan else adhoc_scan
-
-        timestep = self.coupling_timestep
-        total_time = end_time - start_time
-        total_steps = int(total_time / timestep)
         
-        if total_steps * timestep != total_time:
+        total_time = end_time - start_time
+        total_steps = int(total_time / self.coupling_timestep)
+        
+        if total_steps * self.coupling_timestep != total_time:
             raise Exception("timestep has to exactly divide (end_time - start_time).")
 
-        # The goal should be generate forward function once
-        # and reuse it all the time.
-
-        # Currently, atmosphere model output will have strange
-        # shape if reuse the forward function. This causes the
-        # error during post-processing. Therefore for now, I 
-        # fall back to generate forward function every time.
-        #
-        cpl_step_function = coupler.generate_step_function(jitted=jax_scan)
-        final_state, predictions = scan_func(
-            cpl_step_function,
-            init_cplstate,
-            xs=jnp.arange(total_steps),
+        coupled_step_function = self.generate_step_function(jitted=jax_scan)
+        final_coupled_state, predictions = scan_func(
+            coupled_step_function,
+            init_coupled_state,
+            xs = start_time + self.coupling_timestep * jnp.arange(total_steps),
         )
-
         predictions = unwrap_leading_dims(predictions, first_n_dim=2)
 
         _end_time = time.time()
         _elapsed_time = _end_time - _start_time
         print(f"Execution time: {_elapsed_time:.1f} seconds.")
 
-        return final_state, predictions
+        return final_coupled_state, predictions
         
     
     def add_component(
