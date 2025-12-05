16eb3c3a2065819952b1eb66880ccb30446de14e
Tien-Yiao Hsu <tienyiao@pik-potsdam.de>
2025-12-05 12:27:03 +0100

    Import components into jax_gcm.components for simplicity

--- jax_esm/components/__init__.py
+++ jax_esm/components/__init__.py
@@ -1,7 +1,15 @@
 """Earth system components module."""
 
 from jax_esm.components.base import Component
+from jax_esm.components.JCM import JCM
+from jax_esm.components.SlabOceanModel import SlabOceanModel
+from jax_esm.components.SlabAtmosphereModel import SlabAtmosphereModel
+from jax_esm.components.SlabLandModel import SlabLandModel
 
 __all__ = [
     "Component",
+    "JCM",
+    "SlabOceanModel",
+    "SlabAtmosphereModel",
+    "SlabLandModel",
 ]
