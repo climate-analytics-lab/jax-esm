081778de826120cdbe220cb3428c4a0611251533
Tien-Yiao Hsu <meteorologytoday@gmail.com>
2025-12-17 23:41:46 +0100

    Cleanup helper functions

--- jax_esm/components/base.py
+++ jax_esm/components/base.py
@@ -9,7 +9,8 @@ import xarray as xr
 import tree_math
 from dataclasses import make_dataclass
 
-from jax_esm.components.domain import Domain
+from jax_esm.domain import Domain
+from jax_esm.variable_registry import VariableRegistry
 
 
 class AbstractFieldGroup:
@@ -40,201 +41,6 @@ class ComponentState:
     phydata: AbstractFieldGroup | Any
 
 
-def create_field_group_class(
-    cls_name: str,
-    fields: Sequence[Tuple[str, type, Sequence[int]]],
-    base_cls=AbstractFieldGroup,
-):
-    """
-    A tool function that creates a state class dynamically with given dimension.
-    The created class will have the following methods: `zeros`, `ones`, and `copy`.
-
-    Args:
-
-        cls_name : Name of the state class
-        fields   : A list of (varname, data type, shape) tuples.
-
-    Returns:
-
-        class : The resulting state class.
-
-    """
-    dataclass_fields = [(varname, dtype) for varname, dtype, _ in fields]
-
-    cls = make_dataclass(
-        cls_name=cls_name,
-        fields=dataclass_fields,
-        bases=(base_cls,),
-    )
-
-    @classmethod  # type: ignore
-    def zeros(_cls, **kwargs):
-        init_args = dict()
-        for varname, dtype, shape in fields:
-            if (varname in kwargs) and (kwargs[varname] is not None):
-                init_args[varname] = kwargs[varname]
-            else:
-                init_args[varname] = jnp.zeros(shape, dtype=dtype)
-
-        return _cls(**init_args)
-
-    @classmethod  # type: ignore
-    def ones(_cls, **kwargs):
-        init_args = dict()
-        for varname, dtype, shape in fields:
-            if (varname in kwargs) and (kwargs[varname] is not None):
-                init_args[varname] = kwargs[varname]
-            else:
-                init_args[varname] = jnp.ones(shape, dtype=dtype)
-
-        return _cls(**init_args)
-
-    def copy(self, **kwargs):
-        init_args = dict()
-        for varname, dtype, shape in fields:
-            if (varname in kwargs) and (kwargs[varname] is not None):
-                init_args[varname] = kwargs[varname]
-            else:
-                init_args[varname] = getattr(self, varname)
-
-        return type(self)(**init_args)
-
-    cls.zeros = zeros  # type: ignore
-    cls.ones = ones  # type: ignore
-    cls.copy = copy  # type: ignore
-
-    return tree_math.struct(cls)
-
-
-def create_component_state_class(
-    prog_cls: type,
-    phydata_cls: type,
-    cls_name: str = "",
-):
-    """
-    A tool function that creates a ComponentState class dynamically with given dimension.
-    The created class will have the following methods: `zeros`, `ones`, and `copy`.
-
-    Args:
-
-        prog      : The prog class.
-        phydata   : The phydata class.
-        cls_name  : Name of the class.
-
-    Returns:
-
-        The resulting class.
-
-    """
-
-    new_cls = make_dataclass(
-        f"{cls_name:s}",
-        [
-            ("prog", prog_cls),
-            ("phydata", phydata_cls),
-        ],
-        bases=(ComponentState,),
-    )
-
-    @classmethod  # type: ignore
-    def zeros(_cls):
-        return _cls(
-            prog=prog_cls.zeros(),
-            phydata=phydata_cls.zeros(),
-        )
-
-    @classmethod  # type: ignore
-    def ones(_cls):
-        return _cls(
-            prog=prog_cls.ones(),
-            phydata=phydata_cls.ones(),
-        )
-
-    def copy(self, prog_kwargs=None, phydata_kwargs=None):
-        if prog_kwargs is None:
-            prog_kwargs = {}
-        if phydata_kwargs is None:
-            phydata_kwargs = {}
-        o = new_cls(
-            prog=self.prog.copy(**prog_kwargs),
-            phydata=self.phydata.copy(**phydata_kwargs),
-        )
-
-        return o
-
-    new_cls.zeros = zeros  # type: ignore
-    new_cls.ones = ones  # type: ignore
-    new_cls.copy = copy  # type: ignore
-
-    new_cls = tree_math.struct(new_cls)
-
-    return new_cls
-
-
-def create_component_forcing_class(
-    flux_cls: type,
-    scalar_cls: type,
-    cls_name: str = "",
-):
-    """
-    A tool function that creates a ComponentState class dynamically with given dimension.
-    The created class will have the following methods: `zeros`, `ones`, and `copy`.
-
-    Args:
-
-        flux      : The flux class.
-        scalar    : The scalar class.
-        cls_name  : Name of the class.
-
-    Returns:
-
-        The resulting class.
-
-    """
-
-    new_cls = make_dataclass(
-        f"{cls_name:s}",
-        [
-            ("flux", flux_cls),
-            ("scalar", scalar_cls),
-        ],
-        bases=(ComponentForcing,),
-    )
-
-    @classmethod  # type: ignore
-    def zeros(_cls):
-        return _cls(
-            flux=flux_cls.zeros(),
-            scalar=scalar_cls.zeros(),
-        )
-
-    @classmethod  # type: ignore
-    def ones(_cls):
-        return _cls(
-            flux=flux_cls.ones(),
-            scalar=scalar_cls.ones(),
-        )
-
-    def copy(self, flux_kwargs=None, scalar_kwargs=None):
-        if flux_kwargs is None:
-            flux_kwargs = {}
-        if scalar_kwargs is None:
-            scalar_kwargs = {}
-        o = new_cls(
-            flux=self.flux.copy(**flux_kwargs),
-            scalar=self.scalar.copy(**scalar_kwargs),
-        )
-
-        return o
-
-    new_cls.zeros = zeros  # type: ignore
-    new_cls.ones = ones  # type: ignore
-    new_cls.copy = copy  # type: ignore
-
-    new_cls = tree_math.struct(new_cls)
-
-    return new_cls
-
 
 @dataclass
 class CoupledComponentConfig:
@@ -244,10 +50,13 @@ class CoupledComponentConfig:
     timestep: float  # seconds
 
 
-class Component(ABC):
+class CoupledComponent(ABC):
     """Abstract base class for Earth system components."""
 
     component_forcing_class: ComponentForcing
+    component_state_class: ComponentState
+    state_variable_registry: VariableRegistry
+    forcing_variable_registry: VariableRegistry
     domain: Domain
 
     def __init__(self, config: CoupledComponentConfig):
@@ -307,3 +116,4 @@ class Component(ABC):
             and metadata.
         """
         pass
+
