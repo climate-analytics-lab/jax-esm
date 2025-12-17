a7b32f5d113eb53c29b14f69702486963a712494
Tien-Yiao Hsu <meteorologytoday@gmail.com>
2025-12-17 23:37:17 +0100

    Add import

--- jax_esm/__init__.py
+++ jax_esm/__init__.py
@@ -1,6 +1,6 @@
 """JAX-ESM: A JAX-based Earth System Model coupler."""
 
-from jax_esm.components.base import Component, CoupledComponentConfig
+from jax_esm.components.base import Component, CoupledComponentConfig, AbstractFieldGroup, ComponentForcing, ComponentState
 from jax_esm.coupling.coupler import Coupler
 
 __version__ = "0.1.0"
@@ -9,4 +9,7 @@ __all__ = [
     "Component",
     "CoupledComponentConfig",
     "Coupler",
+    "AbstractFieldGroup",
+    "ComponentForcing",
+    "ComponentState",
 ]
