cdd7640981d9ab0fd4f0e25f9a90a2e8a187219f
Tien-Yiao Hsu <meteorologytoday@gmail.com>
2025-12-30 13:34:34 +0100

    Sucessfully create a test case for JCM + GEOFRIC

---
+++ tests/integration/test_jesm_JCM_GEOFRIC.py
@@ -0,0 +1,105 @@
+"""Example of coupling of simple slab atmosphere, ocean, and land models."""
+
+
+def test_integration():
+
+    import jcm
+    from jax_esm.components import JCM
+    from jax_esm.components.GEOFRIC import GEOFRIC
+    from GEOFRIC.model import Model as GEOFRICRawModel
+    import jax_datetime as jdt
+    from pathlib import Path
+    from jax_esm.coupling.transformer import ESMFRegridTransformer
+    from jax_esm.coupling.forcing_mapper import ForcingMapper
+    from jax_esm.coupling.coupler import Coupler
+    resolution = 31
+    grid_specification = f"JCM::T{resolution:d}"
+
+    coupling_timestep = 86400.0
+    start_datetime = jdt.to_datetime("2000-01-01")
+    simulation_interval = jdt.to_timedelta(60, "day")
+    output_dir = Path("output/JCM_GEOFRIC").resolve()
+
+    print("Output dir: ", str(output_dir))
+    output_dir.mkdir(exist_ok=True, parents=True)
+
+    # Creating components
+    components = dict(
+        atm=JCM(
+            model=jcm.model.Model(start_date=start_datetime),
+            coupling_timestep=coupling_timestep,
+            save_interval=coupling_timestep,
+            land_model_active=False,
+        ),
+        ocn = GEOFRIC(
+            model = GEOFRICRawModel(
+                resolution = 20,
+                timestep = coupling_timestep,
+            ),
+            coupling_timestep=coupling_timestep,
+            save_interval=coupling_timestep,
+        ),
+    )
+
+    # Creating transofmrations
+    transformers = dict(
+        a2o = dict(
+            conserve = ESMFRegridTransformer(
+                weight_file = "regrid_weights/weight_algo-conserve_JCM_T31_to_LLC_20.nc",
+                source_grid = components["atm"].domain.horizontal_grids["T"],
+                target_grid = components["ocn"].domain.horizontal_grids["T"],
+            ),
+        ),
+        o2a = dict(
+            bilinear = ESMFRegridTransformer(
+                weight_file = "regrid_weights/weight_algo-bilinear_LLC_20_to_JCM_T31.nc",
+                source_grid = components["ocn"].domain.horizontal_grids["T"],
+                target_grid = components["atm"].domain.horizontal_grids["T"],
+            ),
+        ),
+    )
+
+    forcing_mapper = ForcingMapper(components=components)
+    forcing_mapper.add_forcing_mapping(
+        source = ("atm", "extra.total_heat_flux"),
+        target = ("ocn", "flux.total_heat_flux"),
+        transformer = transformers["a2o"]["conserve"],
+    )
+    forcing_mapper.add_forcing_mapping(
+        source = ("ocn", "prog.sea_surface_temperature"),
+        target = ("atm", "sea_surface_temperature"),
+        transformer = transformers["o2a"]["bilinear"],
+    )
+    
+    # Construct coupled model
+    model = Coupler(
+        components=components,
+        forcing_mapper=forcing_mapper,
+        coupling_timestep=coupling_timestep,
+    )
+
+    # Obtain initial condition
+    initial_coupled_state_forcing = model.initialize()
+   
+    trajectory_function = model.generate_trajectory_function(
+        start_time=0,
+        end_time=simulation_interval / jdt.to_timedelta(1, "second"),
+        jitted=True,
+        show_progress=True,
+        tqdm_kwargs=dict(desc="Simulation"),
+    )
+ 
+    # Run coupled model
+    print("Running model...")
+    state_holder, predictions = trajectory_function(initial_coupled_state_forcing)
+    
+    # Convert output into xarray
+    output_dict = model.predictions_to_xarray(predictions)
+
+    for component_name, ds in output_dict.items():
+        output_file = output_dir / f"{component_name:s}.nc"
+        print("Output file: ", str(output_file))
+        ds.to_netcdf(output_file, engine="netcdf4")
+
+if __name__ == "__main__":
+    test_integration()
