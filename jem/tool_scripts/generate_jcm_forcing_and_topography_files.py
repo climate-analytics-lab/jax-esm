f12d0b1aaa9b6a4d4c904bd2cfb3cdd8cd27ae2a
Tien-Yiao Hsu <meteorologytoday@gmail.com>
2026-01-11 21:26:18 +0100

    Improve the process of interpolation jcm files.

--- jem/tool_scripts/generate_jcm_forcing_and_topography_files.py
+++ jem/tool_scripts/generate_jcm_forcing_and_topography_files.py
@@ -1,33 +1,63 @@
+from typing import Optional, Dict
+import os
+import shutil
+import subprocess
+import sys
 from pathlib import Path
 import jcm
 
+def generate_jcm_forcing_and_topography_files(
+    resolution: int,
+    data_directory: Optional[Path] = None,
+) -> Dict[str, Path]:
 
-def generate_jcm_forcing_and_topography_files(resolution: int = 31):
+    import jcm
+
+    if not (isinstance(data_directory, Path) or data_directory is None):
+        raise TypeError("`data_directory` must be of type `Path` or `None`.")
+
+    if data_directory is None:
+        home_data_directory = os.environ.get("HOME", None)
+
+        if home_data_directory is None:
+            data_directory = Path.cwd()
+        else:
+            data_directory = Path(home_data_directory)
+
+        data_directory = data_directory / ".cache/jcm"
+
+        print(f'Using input data directory: "{str(data_directory)}".')
+
+    raw_data_directory = Path(jcm.__file__).parent / f"data/bc"
+
     # Prepare boundary file
     files_to_check = dict(
-        terrain=(
-            Path(jcm.__file__).parent / f"data/bc/terrain_t{resolution:d}.nc"
-        ).resolve(),
-        forcing=(
-            Path(jcm.__file__).parent / f"data/bc/forcing_t{resolution:d}.nc"
-        ).resolve(),
+        terrain=(data_directory / f"terrain_t{resolution:d}.nc").resolve(),
+        forcing=(data_directory / f"forcing_t{resolution:d}.nc").resolve(),
     )
 
-    interpolation_code = (
-        Path(jcm.__file__).parent / "data/bc/interpolate.py"
-    ).resolve()
+    def check_if_file_exist(
+        file_dict: Dict[str, Path], verbose: bool = True
+    ) -> Dict[Path, bool]:
 
-    def get_files_exist(file_dict):
-        return [Path(file).exists() for _, file in file_dict.items()]
+        file_status = {file: Path(file).exists() for _, file in file_dict.items()}
 
-    for _, file_name in files_to_check.items():
-        print(f"Check file: {str(file_name)}")
+        if verbose:
+            for file, result in file_status.items():
+                print(
+                    f"Check file: {str(file):s}...",
+                    "found." if result else "not found.",
+                )
 
-    if not all(get_files_exist(files_to_check)):
-        print("Some files do not exist. Need to produce it.")
+        return file_status
 
-        import subprocess
-        import sys
+    file_status = check_if_file_exist(files_to_check)
+
+    if not all(list(file_status.values())):
+        print("Some files are missing. Need to generate them.")
+
+        data_directory.mkdir(parents=True, exist_ok=True)
+        interpolation_code = (raw_data_directory / "interpolate.py").resolve()
 
         try:
             result = subprocess.run(
@@ -35,19 +65,21 @@ def generate_jcm_forcing_and_topography_files(resolution: int = 31):
                 check=True,
                 capture_output=True,
                 text=True,
+                cwd=data_directory,
             )
-            print(result.stdout)
         except subprocess.CalledProcessError as e:
             print("Error output:", e.stderr)
-    if all(get_files_exist(files_to_check)):
-        print("All files exist!")
-    else:
-        raise Exception(
-            "Something went wrong. The daily file is not generated. Please check."
-        )
+
+        for destination_file in files_to_check.values():
+            source_file = Path(raw_data_directory / destination_file.name)
+            print(f"Copying: {str(source_file):s} => {str(destination_file):s}")
+            shutil.copy(source_file, destination_file)
+
+        new_file_status = check_if_file_exist(files_to_check)
+        if not all(list(new_file_status.values())):
+            raise FileNotFoundError(
+                "Something went wrong. The daily file is not generated. Please check."
+            )
 
     return files_to_check
 
-
-if __name__ == "__main__":
-    generate_jcm_forcing_and_topography_files()
