73680dd4ac5832c87b9625101b55b5416c5ec912
Tien-Yiao Hsu <meteorologytoday@gmail.com>
2026-01-15 17:16:21 +0100

    Update document

--- jem/coupling/forcing_mapper.py
+++ jem/coupling/forcing_mapper.py
@@ -6,8 +6,17 @@ from jem.coupling.transformer import Transformer
 
 
 class ForcingMapper:
-    """Manages flux exchange and boundary condition translation between components."""
-
+    """Manages flux exchange and boundary condition translation between components.
+    
+    Attributes:
+        component_names: List of component names
+        forcing_mappings: Optional mapping of flux names between components.
+            Keys are (source, target) component pairs, values are dicts 
+            mapping source flux names to target names.
+        transformations: Optional transformations for fluxes.
+            Keys are (source, target, flux_name) tuples, values are transformation
+            functions.
+    """
     components: Dict[str, CoupledComponent]
     forcing_mappings: Dict[Tuple[str, str], Dict[str, str]]
     transformations: Dict[Tuple[str, str, str, str], Callable]
@@ -19,18 +28,7 @@ class ForcingMapper:
         forcing_mappings: Optional[Dict[Tuple[str, str], Dict[str, str]]] = None,
         transformations: Optional[Dict[Tuple[str, str, str, str], Callable]] = None,
     ):
-        """Initialize flux exchanger.
-
-        Args:
-            component_names: List of component names
-            forcing_mappings: Optional mapping of flux names between components.
-                          Keys are (source, target) component pairs,
-                          values are dicts mapping source flux names to target names.
-            transformations: Optional transformations for fluxes.
-                           Keys are (source, target, flux_name) tuples,
-                           values are transformation functions.
-        """
-
+        """Initialize flux exchanger."""
         self.components = components
         self.component_names = list(self.components.keys())
         self.forcing_mappings = forcing_mappings or {}
@@ -72,9 +70,14 @@ class ForcingMapper:
         """Map fluxes and scalars between components.
 
         Args:
+            component_forcings: A dict that maps component name
+                to component forcing class.
+            component_states: A dict that maps component name to
+                component state class.
 
         Returns:
-            Dictionary mapping component names to their input fluxes
+            A dict that maps component names to the resulting
+            forcing obejcts.
         """
 
         for (
@@ -176,7 +179,7 @@ class ForcingMapper:
         Args:
             source_component_name: Source component name
             target_component_name: Target component name
-            mapping: Dictionary mapping source state names to target forcing
+            mapping: A dict mapping source state names to target forcing
         """
         source_component_name, source_variable_name = source
         target_component_name, target_variable_name = target
@@ -205,11 +208,11 @@ class ForcingMapper:
         """Add transformation function for a specific flux.
 
         Args:
-            source: Source component name
-            target: Target component name
+            source_component_name: Source component name
+            target_component_name: Target component name
             source_variable_name: Name of the source variable to transform
             target_variable_name: Name of the target variable to transform
-            transform_fn: Transformation function
+            transformer: A :code:`Transformer` that will be used to regrid the input.
         """
         source_variable_metadata = self.component_state_variable_registries[
             source_component_name
