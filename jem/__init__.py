504700dfcf680edc9554bfbaf4c47cf801aad05d
Tien-Yiao Hsu <tienyiao@pik-potsdam.de>
2026-01-16 15:21:07 +0100

    Update __init__.py to make documentation PRs easier

--- jem/__init__.py
+++ jem/__init__.py
@@ -4,14 +4,31 @@ __version__ = "0.1.0"
 
 from .coupling.coupler import Coupler
 from .coupling.transformer import Transformer
+from .coupling.forcing_mapper import ForcingMapper
 
 from .base.grid import Grid
 from .base.grid import GridSpecification
 
+from .base.domain import Domain
+from .base.variable import (
+    VariableMetadata,
+    VariableRegistry,
+)
+
+from .base.data_structure import (
+    typed_and_dimensioned,
+    build_dataclass_from_typed_and_dimensioned,
+)
 
 __all__ = [
     "Transformer",
+    "ForcingMapper",
     "IdentityTransformer",
     "Grid",
     "GridSpecification",
+    "Domain",
+    "VariableRegistry",
+    "VariableMetadata",
+    "typed_and_dimensioned",
+    "build_dataclass_from_typed_and_dimensioned",
 ]
