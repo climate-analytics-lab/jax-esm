11bdff66c58e1a357b2dca722e0c0e8263529a2d
Tien-Yiao Hsu <tienyiao@pik-potsdam.de>
2026-01-22 13:58:07 +0100

    Add in typing system

---
+++ jem/base/typing.py
@@ -0,0 +1,26 @@
+from typing import TypeVar, Any, Callable, List
+import jax.numpy as jnp
+import jax_datetime as jdt
+import xarray as xr
+
+Array = jnp.ndarray
+ArrayOrArrayTuple = Array | tuple[Array, ...]
+Numeric = float | int | Array
+
+Pytree = Any
+
+CalendarTimeStr = str
+State = Pytree
+Forcing = Pytree
+SimulationTime = float
+
+History = TypeVar('History')
+JittableFlag = bool
+
+InitializeFunction = Callable[[CalendarTimeStr], tuple[State, Forcing]]
+
+StepFunction = Callable[[State, Forcing, SimulationTime], tuple[State, History]]
+StepFunctionGenerator = Callable[[JittableFlag], StepFunction]
+
+HistoryToXarray = Callable[History, xr.Dataset]
+
