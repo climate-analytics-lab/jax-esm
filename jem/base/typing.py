65f72dc7c90fee028606fdce31d2a08c67fc8d49
Tien-Yiao Hsu <meteorologytoday@gmail.com>
2026-01-25 00:09:28 +0100

    Add JEMComponent and JEMForcingMapper as an internal protocol class

--- jem/base/typing.py
+++ jem/base/typing.py
@@ -2,14 +2,16 @@ from typing import TypeVar, Any, Callable, List, Dict
 import jax.numpy as jnp
 import jax_datetime as jdt
 import xarray as xr
+from dataclasses import dataclass
 
 Array = jnp.ndarray
 ArrayOrArrayTuple = Array | tuple[Array, ...]
 Numeric = float | int | Array
 
-Pytree = Any
+ComponentName = str
 
-CalendarTimeStr = str
+Pytree = Any
+Workflow = Pytree
 State = Pytree
 Forcing = Pytree
 SimulationTime = float
@@ -25,5 +27,37 @@ StepFunctionGenerator = Callable[[JittableFlag], StepFunction]
 HistoryToXarray = Callable[History, xr.Dataset]
 
 GetInfoFunction = Callable[[], Dict]
+
+
+ForcingMapperFunction = Callable[ [ Dict[str, Forcing], Dict[str, State] ], Dict[str, Forcing] ]
+RegridderFunction = Callable[[Array], Array]
+
+@dataclass
+class JEMComponent:
+    # skipped
+    raw_component: Any
+    name: str
+   
+    # mandatory
+    component_state_class : State
+    component_forcing_class : Forcing
+    state_variable_registry : Any
+    forcing_variable_registry : Any
+    initialize : InitializeFunction
+    generate_step_function : StepFunctionGenerator
+    predictions_to_xarray : HistoryToXarray
+    get_info : GetInfoFunction
+
+
+@dataclass
+class JEMForcingMapper:
+    # skipped
+    raw_forcing_mapper: Any
+    name: str
+
+    # mandatory
+    involved_component_names: List[str]
+    get_info : GetInfoFunction
+    map_forcings: ForcingMapperFunction
 
 
