1eac58ed725e46b6180ad451b2fc14e6ebab7d7a
Tien-Yiao Hsu <meteorologytoday@gmail.com>
2026-01-25 00:08:31 +0100

    Make mixing detection more sophisticated

--- jem/base/mixin.py
+++ jem/base/mixin.py
@@ -1,5 +1,5 @@
 from typing import get_args, get_type_hints, get_origin
-from typing import Any, Callable, Dict, Optional
+from typing import Any, Callable, Dict, List, Optional
 import inspect
 
 
@@ -17,52 +17,78 @@ class MethodSignatureNotMatchError(Exception):
 
 def verify(
     target : Any,
-    reference_class: Optional[type] = None,
-    metadata: Optional[Dict[str, type]] = None,
+    reference_class: type,
     verbose: bool = False,
-) -> None:
-    if reference_class is None and metadata is None:
-        raise ValueError("Either reference_class or metadata must be given.")
-    if reference_class is not None:
-        metadata = get_type_hints(reference_class)
+    customized_mapping_name:str = "__JEM_CUSTOMIZED_MAPPING__",
+    skip: Optional[ str | List[str] ] = [],
+) -> Dict[str, Any]:
+    
+    metadata = get_type_hints(reference_class)
+    customized_mapping = getattr(target, customized_mapping_name, {})
+    if isinstance(skip, str):
+        skip = [skip,]
+    result = {}
     for member_name, member_type in metadata.items():
+        verified_name = None
+        if member_name in skip:
+            continue
         if verbose:
             print(f"Checking `{member_name:s}` => `{str(member_type)}`")
         if get_origin(member_type) is get_origin(Callable):
-            _verify_function(target, member_name, member_type)
+            verified_name = _verify_function(target, member_name, member_type, customized_mapping)
         else:
-            _verify_member(target, member_name, member_type)
+            verified_name = _verify_member(target, member_name, member_type, customized_mapping)
 
+        result[verified_name] = getattr(target, member_name)
+
+    return result
 
 def _verify_function(
     target : Any,
     function_name: str,
     function_signature: Callable,
-    verbose: bool = False,
-) -> None:
+    customized_mapping: Dict[str, Any],
+    verbose: bool = True,
+) -> str:
 
     if not isinstance(function_signature, Callable):
         raise ValueError(f"The function signature of `{function_name:s}` should be a Callable. ")
     
-    if not hasattr(target, function_name):
+    if (function_name not in customized_mapping) and (not hasattr(target, function_name)):
         raise MethodNotFoundError(f"The method `{function_name:s}` is not found")
 
+    if function_name in customized_mapping:
+        remapped_function_name = customized_mapping[function_name]
+        if verbose:
+            print(f"Customized mapping found: `{function_name}` is mapped to `{remapped_function_name}`")
+        function_name = remapped_function_name
+    
     target_function_sigature = inspect.signature(getattr(target, function_name))
     goal_argument_types, output_type = get_args(function_signature)
 
     if len(target_function_sigature.parameters.keys()) != len(goal_argument_types):
         raise MethodSignatureNotMatchError(f"The number of parameters in the method `{function_name:s}` ({len(target_function_sigature.parameters.keys()):d}) is not the same as given `{str(function_signature)}` ({len(goal_argument_types):d})")
 
+    return function_name
+
 def _verify_member(
     target : Any,
     member_name: str,
     member_type: type,
-) -> None:
+    customized_mapping: Dict[str, Any],
+    verbose: bool = True,
+) -> str:
 
-    if not hasattr(target, member_name):
+    if (member_name not in customized_mapping) and (not hasattr(target, member_name)):
         raise MemberNotFoundError(f"The member `{member_name:s}` is not found")
 
+    if member_name in customized_mapping:
+        remapped_member_name = customized_mapping[member_name]
+        if verbose:
+            print(f"Customized mapping found: `{member_name}` is mapped to `{remapped_member_name}`")
+        member_name = remapped_member_name
+     
     member = getattr(target, member_name)
-    if member_type != Any and not isinstance(member, member_type):
+    if member_type != Any and not isinstance(member, get_origin(member_type) or member_type):
         raise MemberTypeNotMatchError(f"The member `{member_name:s}` ({str(type(member))}) is expected to be of type `{str(member_type)}`.")
-
+    return member_name
