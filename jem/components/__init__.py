9aa80de022bd093014aa99cba31735da6c115627
Tien-Yiao Hsu <meteorologytoday@gmail.com>
2026-01-25 19:18:16 +0100

    Integrate import

--- jem/components/__init__.py
+++ jem/components/__init__.py
@@ -4,10 +4,12 @@ from jem.components.JCM import JCM
 from jem.components.slab_atmosphere_model import SlabAtmosphereModel
 from jem.components.slab_ocean_model import SlabOceanModel
 from jem.components.slab_land_model import SlabLandModel
+from jem.components.land_model import LandModel
 
 __all__ = [
     "JCM",
     "SlabLandModel",
     "SlabAtmosphereModel",
     "SlabOceanModel",
+    "LandModel",
 ]
